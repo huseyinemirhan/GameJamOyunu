24d8f5820f2be9cd5fa62b41d497411d9b03daca
qeFLilyum <96309517+qeFLilyum@users.noreply.github.com>
2022-05-07 16:11:12 +0300

    gfgf
    gfgf

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
   public float MovementSpeed;
   public float JumpForce;
+  public Animator animator;
   private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -19,14 +20,11 @@ public class PlayerController : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position  += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
 
+        animator.SetFloat("Speed", movement);
+
         if(Input.GetKeyDown("up") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
-        }
-
-        if(!Mathf.Approximately(0,movement))
-        {
-            transform.rotation = movement > 0 ? Quaternion.Euler(0,180,0) : Quaternion.identity;
         }
     }
 }
